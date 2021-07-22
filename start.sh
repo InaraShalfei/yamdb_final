f3b89802dea70d6e7dcdfea2956a774ced81a082
Инара <inarashugaikina@gmail.com>
2021-07-22 14:50:41 +0600

    Add commands for migrations into start.sh-changes-2

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#python manage.py makemigrations categories, reviews, users
+python manage.py makemigrations users
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
