718a6eea22835143b2989f0b5f087d6d1ce2583c
Инара <inarashugaikina@gmail.com>
2021-07-22 15:50:35 +0600

    Add fixtures.json and delete command for superuser in start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,4 @@
 python manage.py makemigrations users
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-python manage.py createsuperuser
 gunicorn api_yamdb.wsgi:application --bind 0.0.0.0:8000
